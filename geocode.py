ca417b2a0828f38d889a761def3a97e8a43dc284
xopclabs <xopclabs@gmail.com>
2020-04-01 06:48:39 +0300

    Improved readablity; changed config column name

--- geocode.py
+++ geocode.py
@@ -20,7 +20,7 @@ FILENAME = args.merge
 
 def filter_df(df):
     with open(os.path.join('data', TOPICNAME, 'config.yaml'), 'r', encoding='utf-8') as f:
-        filter_words = yaml.load(f, Loader=yaml.FullLoader)['filter']
+        filter_words = yaml.load(f, Loader=yaml.FullLoader)['exclude']
     return df[~df.title.str.lower().str.contains('|'.join(filter_words))]
 
 
@@ -77,27 +77,34 @@ if __name__ == '__main__':
     else:
         filepath = os.path.join(topicfolder, TOPICNAME + '.csv')
     df = pd.read_csv(filepath)
+
     # Dropping NA
     print("Dropping NaN's...")
     df = df[df['price'].notna()]
+
     # Converting parsed_at to datetime
     print('Converting parsed_at to datetime...')
     df['parsed_at'] = pd.to_datetime(df.parsed_at)
+
     # Filtering by list of words
     print('Filtering by title...')
     df = filter_df(df)
+
     # Converting dist_to_metro to float
     print('Converting dist_to_metro to float...')
     df['dist_to_metro'] = df['dist_to_metro'].map(dist_to_float)
+
     # Converting added_time to datetime
     print('Converting added_time to datetime...')
     df['added_time'] = df.apply(lambda x: str_to_datetime(x['added_time'], x['parsed_at']), axis=1)
+
     # Getting latitude and longitude using Yandex API
     print('Using Yandex API to get latitude and longitute...')
     geo = gpd.tools.geocode(df.address, provider='yandex', api_key=API_KEY, timeout=5)
     df = gpd.GeoDataFrame(df)
     df['geometry'] = geo.geometry
     df['address'] = geo.address
+
     # If dataframes need to be merged
     if FILENAME:
         print('Merging with previous dataframe...')
@@ -105,7 +112,10 @@ if __name__ == '__main__':
         df_['parsed_at'] = pd.to_datetime(df_.parsed_at)
         df_['added_time'] = pd.to_datetime(df_.added_time)
         df = df.append(df_, ignore_index=True)
+
+    # Deleting .csv file
     os.remove(filepath)
+    
     # Saving results
     print('Saving...')
     df.to_file(outputfile, driver='GPKG')
